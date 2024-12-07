fc79a895fbcf4c62b411234746c444e874de4d33
Vo Dinh <vo.dinh@alpaca.vn>
2024-12-07 21:28:00 +0700

    Add name, status for searching

--- src/main/java/com/vodinh/prime/requests/WeightRequest.java
+++ src/main/java/com/vodinh/prime/requests/WeightRequest.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 public class WeightRequest {
 
     private Long id;
+    private String name;
 
     @NotBlank
     private String serialNumber;
@@ -24,6 +25,8 @@ public class WeightRequest {
 
     @NotBlank
     private Double quyCach;
+
+    private String status;
 
     private LocalDateTime sellAt;
 
