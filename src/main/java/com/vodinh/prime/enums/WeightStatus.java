cb8c0ba1bcc511ffd7b0c1801d637b3d52759bdf
Vo Dinh <vo.dinh@alpaca.vn>
2024-12-22 21:32:02 +0700

    fix JWT include user

--- src/main/java/com/vodinh/prime/enums/WeightStatus.java
+++ src/main/java/com/vodinh/prime/enums/WeightStatus.java
@@ -1,9 +1,27 @@
 package com.vodinh.prime.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum WeightStatus {
-    RUNNING,
     ERROR,
+    INACTIVE,
     PAUSE,
-    ACTIVE,
-    INACTIVE
+    RUNNING,
+    ACTIVE;
+
+    @JsonCreator
+    public static WeightStatus fromValue(String value) {
+        for (WeightStatus status : WeightStatus.values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid value for WeightStatus: " + value);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
